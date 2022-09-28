1b88c602b30769244dc23b8e952210946b48580a
Sarah Stoffel <17sarah.stoffel@gmail.com>
2022-09-27 21:43:35 -0500

    bubbleplot done

--- static/js/app.js
+++ static/js/app.js
@@ -13,7 +13,7 @@ function DrawBargraph(sampleId) {
 
         let otu_ids = result.otu_ids;
         let otu_labels= result.otu_labels;
-        let sample_values = result.sample_values
+        let sample_values = result.sample_values;
 
         //create trace object
         let barData = {
@@ -39,10 +39,48 @@ function DrawBargraph(sampleId) {
     });
 
 };
-
+//creating a function to draw the bubble plot
 function DrawBubblePlot(sampleId) {
-    //test
-    console.log(`DrawBubblePlot ${sampleId}`);
+    
+    d3.json(url).then(data=> {
+
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleId);
+        let result = resultArray[0]
+
+        let otu_ids = result.otu_ids;
+        let otu_labels= result.otu_labels;
+        let sample_values = result.sample_values;
+
+        //create trace object
+        let bubbleData = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+            
+        };
+
+        //trace => array
+        let bubbleArray = [bubbleData];
+
+        //layout object
+        let bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            margin: {t: 30},
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"}
+        };
+
+        //Plotly function
+        Plotly.newPlot("bubble", bubbleArray, bubbleLayout);
+
+    });
 
 };
 
